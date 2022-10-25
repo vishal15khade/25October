977cf55ed8af48668927936aab1b88f2f2b6c929
vishal_khade <vishal.t.15khade@gmail.com>
2022-10-25 20:29:26 +0530

    commiting 1st time

---
+++ src/demo/Test.java
@@ -0,0 +1,17 @@
+package demo;
+class Test {
+
+	public static void main(String[] args) {
+
+		String hungry="yes"; 
+	if(hungry=="yes")
+	{
+		System.out.println("Eat something...");
+	}
+	else
+	{
+		System.out.println("Do work...");
+	}
+
+	}
+}
